93c9e55302efc4cf005ef20840af04f693351544
Vitaly <minsvital@mail.ru>
2025-02-09 09:47:09 +0400

    Продолжение изучения Spring. Создание 2го модуля

---
+++ manager-app/src/main/java/ag/selm/manager/controller/ProductsController.java
@@ -0,0 +1,42 @@
+package ag.selm.manager.controller;
+
+import ag.selm.manager.client.BadRequestException;
+import ag.selm.manager.client.ProductsRestClient;
+import ag.selm.manager.controller.payload.NewProductPayload;
+import ag.selm.manager.entity.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("catalogue/products")
+public class ProductsController {
+
+    private final ProductsRestClient productsRestClient;
+
+    @GetMapping(value = "list")
+    public String getProductsList(Model model) {
+        model.addAttribute("products", this.productsRestClient.findAllProducts());
+        return "catalogue/products/list";
+    }
+
+    @GetMapping("create")
+    public String getProductPage() {
+        return "catalogue/products/new_product";
+    }
+
+    @PostMapping("create")
+    public String createProduct(NewProductPayload payload, Model model) {
+        try {
+            Product product = this.productsRestClient.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.id());
+        } catch (BadRequestException exception) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", exception.getErrors());
+            return "catalogue/products/new_product";
+        }
+    }
+}
