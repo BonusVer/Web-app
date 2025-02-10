444e7883aa97c98cb50e0650d867a972ee8fb283
Vitaly <minsvital@mail.ru>
2025-02-10 20:22:55 +0400

    Продолжение изучения Spring. Подключение к БД

--- catalogue-service/src/main/java/ag/selm/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ag/selm/catalogue/repository/ProductRepository.java
@@ -1,16 +1,11 @@
 package ag.selm.catalogue.repository;
 
 import ag.selm.catalogue.entity.Product;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductRepository {
-    List<Product> findAll();
+public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-    Product save(Product product);
-
-    Optional<Product> findById(Integer productId);
-
-    void deleteById(Integer id);
 }
