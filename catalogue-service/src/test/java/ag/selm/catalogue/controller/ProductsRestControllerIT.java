d7eed283d9c6dcea83457c76ef88c7be70dbbb3e
Vitaly <minsvital@mail.ru>
2025-02-17 18:00:14 +0400

    Изучение spring тестирование 1 часть

--- catalogue-service/src/test/java/ag/selm/catalogue/controller/ProductsRestControllerIT.java
+++ catalogue-service/src/test/java/ag/selm/catalogue/controller/ProductsRestControllerIT.java
@@ -11,6 +11,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Locale;
+
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -59,7 +61,7 @@ class ProductsRestControllerIT {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("""
                         {"title": "Еще один товар", "details": "Какое-то описание товара"}""")
-                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+                .with(jwt().jwt(builder -> builder.claim("scope", "view_catalogue")));
 
         //when
 
@@ -78,6 +80,55 @@ class ProductsRestControllerIT {
                                      "title": "Еще один товар",
                                      "details": "Какое-то описание товара"
                                  }"""));
+    }
+
+    @Test
+    void createProduct_RequestIsInvalid_ReturnsProblemDetail() throws Exception {
+        //given
+
+        var requestBuilder = MockMvcRequestBuilders.post("/catalogue-api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "ww", "details": null}""")
+                .locale(Locale.of("ru", "RU"))
+                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+
+        //when
+
+        this.mvc.perform(requestBuilder)
+
+                //then
+
+                .andDo(print())
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                 {
+                                   "errors": ["Название товара должно быть от 3 до 50"]
+                                 }"""));
+    }
+
+    @Test
+    void createProduct_UserIsNotAuthorized_ReturnsForbidden() throws Exception {
+        //given
+
+        var requestBuilder = MockMvcRequestBuilders.post("/catalogue-api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "ww", "details": null}""")
+                .locale(Locale.of("ru", "RU"))
+                .with(jwt().jwt(builder -> builder.claim("scope", "view_catalogue")));
+
+        //when
+
+        this.mvc.perform(requestBuilder)
+
+                //then
+
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden());
     }
   
 }
