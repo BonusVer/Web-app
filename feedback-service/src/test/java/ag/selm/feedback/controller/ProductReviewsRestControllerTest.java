ed921fb585f12d6c1c9b7dde8fc9fcb0701a3339
Vitaly <minsvital@mail.ru>
2025-03-07 17:35:46 +0400

    Изучение spring реактивное веб приложение Написание тестов

--- feedback-service/src/test/java/ag/selm/feedback/controller/ProductReviewsRestControllerTest.java
+++ feedback-service/src/test/java/ag/selm/feedback/controller/ProductReviewsRestControllerTest.java
@@ -1,11 +1,15 @@
 package ag.selm.feedback.controller;
 
 import ag.selm.feedback.entity.ProductReview;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -25,17 +29,24 @@ class ProductReviewsRestControllerTest {
     @Autowired
     ReactiveMongoTemplate reactiveMongoTemplate;
 
-    @Test
-    void findProductReviewsByProductId_ReturnsReviews() {
-        //when
+    @BeforeEach
+    void setUp() {
         this.reactiveMongoTemplate.insertAll(List.of(
                         new ProductReview(UUID.fromString("bd7779c2-cb05-11ee-b5f3-df46f1249898"), 1, 1, "Отзыв №1", "user-1"),
                         new ProductReview(UUID.fromString("be424abc-cb05-11ee-ab16-2b747e61f570"), 1, 3, "Отзыв №2", "user-2"),
                         new ProductReview(UUID.fromString("be77f95a-cb05-11ee-91a3-1bdc94fa9de4"), 1, 5, "Отзыв №3", "user-3")
                 ))
                 .blockLast();
+    }
 
-        //then
+    @AfterEach
+    void tearDown() {
+        this.reactiveMongoTemplate.remove(ProductReview.class).all().block();
+    }
+
+    @Test
+    void findProductReviewsByProductId_ReturnsReviews() {
+
         this.webTestClient.mutateWith(mockJwt())
                 .get()
                 .uri("/feedback-api/product-reviews/by-product-id/1")
@@ -48,9 +59,72 @@ class ProductReviewsRestControllerTest {
                           {"id" : "be424abc-cb05-11ee-ab16-2b747e61f570", "productId" : 1, "rating" : 3, "review" : "Отзыв №2", "userId" : "user-2"},
                           {"id" : "be77f95a-cb05-11ee-91a3-1bdc94fa9de4", "productId" : 1, "rating" : 5, "review" : "Отзыв №3", "userId" : "user-3"}
                       ]""");
+    }
 
+    @Test
+    void createProductReview_RequestIsValid_ReturnsCreatedProductReview() {
+        //when
+        this.webTestClient.mutateWith(mockJwt().jwt(builder -> builder.subject("user-tester")))
+                .post()
+                .uri("/feedback-api/product-reviews")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                            "productId" : 1,
+                            "rating" : 5,
+                            "review" : "Отлично!!!"
+                        }""")
+        //then
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().exists(HttpHeaders.LOCATION)
+                .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)
+                .expectBody()
+                .json("""
+                        {
+                            "productId" : 1,
+                            "rating" : 5,
+                            "review" : "Отлично!!!",
+                            "userId": "user-tester"
+                        }""").jsonPath("$.id").exists();
+    }
 
-        //
+    @Test
+    void createProductReview_RequestIsInvalid_ReturnsBadRequest() {
+        //when
+        this.webTestClient.mutateWith(mockJwt().jwt(builder -> builder.subject("user-tester")))
+                .post()
+                .uri("/feedback-api/product-reviews")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("""
+                        {
+                            "productId" : null,
+                            "rating" : -2,
+                            "review" : "Отлично!!!"
+                        }""")
+                //then
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectHeader().doesNotExist(HttpHeaders.LOCATION)
+                .expectHeader().contentTypeCompatibleWith(MediaType.APPLICATION_PROBLEM_JSON)
+                .expectBody()
+                .json("""
+                        {
+                            "errors" : [
+                              "Товар не указан",
+                              "Оценка меньше 1"
+                            ]
+                        }""");
+    }
+
+    @Test
+    void findProductReviewsByProductId_UserIsNotAuthenticated_ReturnsNotAuthorized() {
+
+        this.webTestClient
+                .get()
+                .uri("/feedback-api/product-reviews/by-product-id/1")
+                .exchange()
+                .expectStatus().isUnauthorized();
     }
 
 }
